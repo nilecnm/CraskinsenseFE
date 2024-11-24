a220efe1a7f54f61a0618153477f3c60ab1c4a2b
nilecnm <chanametac@gmail.com>
2024-11-24 23:32:31 +0700

    fix scatter plot2

--- src/components/stepthree.tsx
+++ src/components/stepthree.tsx
@@ -158,7 +158,7 @@ export default function StepThree({ photo, result }: Props) {
                     <img
                       src={"data:image/png;base64, " + result.density_plot}
                       alt="Density plot"
-                      className="object-contain rounded-lg w-2/3 h-2/3 mx-auto"
+                      className="object-contain rounded-lg w-full h-full mx-auto sm:w-2/3 h-2/3"
                     />
                   )}
                 </div>
